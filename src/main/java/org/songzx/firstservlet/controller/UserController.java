70ec50138ed6372e5c4cb41573046a3cfc7e3263
songzxDev <songzx_2326@163.com>
2018-08-19 20:52:54 +0800

    初步实现模糊匹配

--- src/main/java/org/songzx/firstservlet/controller/UserController.java
+++ src/main/java/org/songzx/firstservlet/controller/UserController.java
@@ -2,6 +2,7 @@ package org.songzx.firstservlet.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Method;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.songzx.firstservlet.model.User;
 import org.songzx.firstservlet.service.UserServiceI;
 import org.songzx.firstservlet.service.UserServiceImpl;
@@ -25,31 +27,46 @@ import com.alibaba.fastjson.JSON;
  *         问问自己，想要怎样的人生，静心学习，耐心沉淀，送给自己，共勉。
  *         </p>
  */
-@WebServlet(name = "userController", urlPatterns = "/user/getUserById", asyncSupported = true)
+@WebServlet(name = "userController", urlPatterns = "/user/*", asyncSupported = true)
 public class UserController extends HttpServlet {
 
 	private static final long serialVersionUID = -4517082409888184884L;
+	private static Logger logger = Logger.getLogger(UserController.class);
 	private UserServiceI userService = new UserServiceImpl();
 
 	@Override
-	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		req.setCharacterEncoding("UTF-8");
-		String uid = req.getParameter("uid");
-		String jsonStr = "";
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 获取请求的URI地址信息
+		String url = request.getRequestURI();
+		// 截取其中的方法名
+		String methodName = url.substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."));
+		Method method = null;
 		try {
-			User user = userService.getUserById(uid);
-			if (user != null) {
-				jsonStr = JSON.toJSONString(user);
-			}
+			// 使用反射机制获取在本类中声明了的方法
+			method = getClass().getDeclaredMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
+			// 执行方法
+			method.invoke(this, request, response);
 		} catch (Exception e) {
-			e.printStackTrace();
+			throw new RuntimeException("调用方法出错！");
 		}
-		res.setCharacterEncoding("UTF-8");
-		res.setContentType("application/json");
-		PrintWriter out = res.getWriter();
-		System.out.println(jsonStr);
-		out.write(jsonStr);
-		out.close();
 	}
 
+	protected void getUserById(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String uid = request.getParameter("uid");
+		PrintWriter out = null;
+		try {
+			User user = userService.getUserById(uid);
+			String jsonStr = user != null ? JSON.toJSONString(user) : "";
+			response.setContentType("application/json");
+			out = response.getWriter();
+			out.write(jsonStr);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+		}
+	}
 }
